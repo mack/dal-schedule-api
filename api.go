3fc3f541c0cadb0bada37d51529d00891fbb92a6
Mackenzie Boudreau <mack.boudreau2@gmail.com>
2017-07-17 14:21:13 -0300

    log connections, new api path, and take in GET request

--- api.go
+++ api.go
@@ -4,26 +4,21 @@ import (
   "net/http"
   "fmt"
   "dal-api/db"
+  "time"
 )
 
 func main() {
-  fmt.Printf("Hello, world. \n")
-  http.HandleFunc("/api", welcome)
+  fmt.Printf("API has started running on port: 8080... \n")
   http.HandleFunc("/api/courses", courses)
-  http.HandleFunc("/api/course_list", courseList)
   http.ListenAndServe(":8080", nil)
 }
 
-func welcome(w http.ResponseWriter, r *http.Request) {
+func courses(w http.ResponseWriter, r *http.Request) {
+  t := time.Now()
+	tn := t.Format("2006/01/02 15:04:05")
+  fmt.Printf("[" + tn + "] New connection made")
   w.Header().Set("Access-Control-Allow-Origin", "*")
   w.Header().Set("Content-Type", "application/json")
-  fmt.Fprintf(w, string(db.Open("CSCI")))
-}
-
-func courses(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "Return JSON format")
-}
-
-func courseList(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "Return JSON format")
+  category := r.URL.Query().Get("s")
+  fmt.Fprintf(w, string(db.Open(category)))
 }
