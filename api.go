e575612751d443fc15dc7662d9034565c4a45605
Mackenzie Boudreau <mack.boudreau2@gmail.com>
2017-07-17 11:56:04 -0300

    added header to api response

--- api.go
+++ api.go
@@ -15,6 +15,7 @@ func main() {
 }
 
 func welcome(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Access-Control-Allow-Origin", "*")
   w.Header().Set("Content-Type", "application/json")
   fmt.Fprintf(w, string(db.Open("CSCI")))
 }
